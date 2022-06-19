8072abb131b95e092ede455388c90f1e7acb9acb
Fernando Nazario <fernandonazario4@gmail.com>
2022-06-19 15:04:58 -0300

    Small adjustments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css'
 import { Cards } from './components/cards/cards'
 
 function App() {
-  return (
-    <div className="container">
-      <Cards />
-    </div>
-  )
+  return <Cards />
 }
 
 export default App
